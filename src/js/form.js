6f21deaa18a2f14c066cf083db4ecaf966675b7f
Tomek Zawadzki <tomekzawadzki1990@gmail.com>
2022-06-06 11:14:15 +0200

    add dark mode

--- src/js/form.js
+++ src/js/form.js
@@ -8,7 +8,25 @@ hamburger.addEventListener("click", () => {
   nav.classList.toggle("hamburger--open");
 });
 
-console.log(moment())
+console.log(moment());
 
-const date = document.querySelector(".date--js")
-date.innerHTML = moment().format('LLLL')
+const date = document.querySelector(".date--js");
+date.innerHTML = moment().format("LLLL");
+
+let isDark = false;
+const button = document.querySelector(".switch__button--js");
+
+button.addEventListener("click", () => {
+  if (isDark) {
+    document.documentElement.style.setProperty("--text-color", "white");
+    document.documentElement.style.setProperty("--background-color", "rgb(7, 7, 122)");
+    document.documentElement.style.setProperty("--header-color", "black");
+    isDark = false;
+  } else {
+    document.documentElement.style.setProperty("--text-color", "rgb(7, 7, 122)");
+    document.documentElement.style.setProperty("--background-color", "white");
+    document.documentElement.style.setProperty("--header-color", "white");
+
+    isDark = true;
+  }
+});
