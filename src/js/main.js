3b80d5179753b9221da502bf3c31f73e5ee2ee7c
tomek-zawadzki <65092372+tomek-zawadzki@users.noreply.github.com>
2022-06-16 16:40:27 +0200

    add my repozitories from github api

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,4 @@
 import "../scss/main.scss";
-
 console.log("Hello Tomcio");
 
 const coachText = document.querySelector(".coach__carleto--text-js");
@@ -56,3 +55,19 @@ hamburger.addEventListener("click", () => {
   const nav = document.querySelector(".hamburger--js");
   nav.classList.toggle("hamburger--open");
 });
+
+fetch("https://api.github.com/users/tomek-zawadzki/repos")
+  .then((resp) => resp.json())
+  .then((resp) => {
+    for (let repo of resp) {
+      const { name, html_url } = repo;
+      console.log(`${name} ${html_url}`);
+      const repositoryList = document.querySelector(".repos-list--js");
+      const myTemplate = `<li> ${name} <a href="${html_url}" title="link do repozytorium na GitHubie" 
+            style="color: #e0ff4f">link do GitHuba</a></li>`;
+      repositoryList.innerHTML += myTemplate;
+    }
+  })
+  .catch((error) => {
+    console.log("error");
+  });
